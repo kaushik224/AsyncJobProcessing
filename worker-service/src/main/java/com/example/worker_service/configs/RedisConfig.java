585262c75a28882ee5924e5f40c925fe336942d1
Kaushik <kaushik19231@gmail.com>
2026-04-18 18:30:00 +0530

    feat(config): configure RedisTemplate for worker-service locking

---
+++ worker-service/src/main/java/com/example/worker_service/configs/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.example.worker_service.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+@Configuration
+public class RedisConfig {
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
+    
+    template.setKeySerializer(new StringRedisSerializer());
+    template.setHashKeySerializer(new StringRedisSerializer());
+    
+    template.setValueSerializer(new JdkSerializationRedisSerializer());
+    template.setHashValueSerializer(new JdkSerializationRedisSerializer());
+    
+    template.afterPropertiesSet();
+    return template;
+  }
+
+}
